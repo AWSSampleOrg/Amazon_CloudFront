910757ff5aabfbfce00982f161d4454f8f54dbb7
Yo-mah-Ya <shanzhongziye@gmail.com>
2024-04-21 21:58:56 -0400

    added OAI

--- ConfiguringSecureAccessAndRestrictingAccessToContent/RestrictingAccessToAWSOrigin/OAI/deploy.sh
+++ ConfiguringSecureAccessAndRestrictingAccessToContent/RestrictingAccessToAWSOrigin/OAI/deploy.sh
@@ -2,9 +2,12 @@
 
 profile="default"
 STACK_NAME="CDN"
+S3_BUCKET=$(whoami)-$(date +%Y-%m-%d)
 
 aws cloudformation deploy \
     --template-file template.yml \
     --stack-name ${STACK_NAME} \
+    --parameter-overrides \
+    BucketName=${S3_BUCKET} \
     --capabilities CAPABILITY_NAMED_IAM \
     --profile ${profile}
